b50a46038951c83abe8ac85b78ddd5c8fa57a883
rahul0218 <kumar.rahul0218@gmail.com>
2022-02-07 21:58:36 +0530

    incomplete insert

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -6,7 +6,7 @@
             }
 
             $data['title'] = ucfirst($page);
-
+            $data['adqry'] = $this->pages_model->get_adqry();
             $this->load->view('templates/header');
             $this->load->view('pages/'.$page, $data);
             $this->load->view('templates/footer');
@@ -14,13 +14,23 @@
 
         public function admission_contact(){
             $data['title'] = 'Create Admission Query';
-
+            $data['adqry'] = $this->pages_model->get_adqry();
             $this->form_validation->set_rules('name','Name','required');
             $this->form_validation->set_rules('phone','Phone','required');
             $this->form_validation->set_rules('course','Course','required');
-
-            $this->load->view('templates/header');
-            $this->load->view('pages/admission_contact', $data);
-            $this->load->view('templates/footer');
+            $this->form_validation->set_rules('message','Message','required');
+            
+            if($this->form_validation->run() === FALSE){
+                $this->load->view('templates/header');
+                $this->load->view('pages/admission_contact', $data);
+                $this->load->view('templates/footer');
+                echo 'this is working';
+            }else{
+                echo 'this is working';
+                $this->pages_model->create_adqry();
+                redirect('pages');
+            }
+            
+            
         }
     }
